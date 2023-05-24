5635912522132d1888b0d787502979c9f3fbb9e5
MinatoHikari <35342316+MinatoHikari@users.noreply.github.com>
2023-05-24 23:12:36 +0800

    fix snapshot module

--- src/snapshot-item/snapshot-item.module.ts
+++ src/snapshot-item/snapshot-item.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SnapshotItemService } from './snapshot-item.service';
 import { SnapshotItemController } from './snapshot-item.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [SnapshotItemController],
-  providers: [SnapshotItemService]
+  providers: [SnapshotItemService, PrismaService],
 })
 export class SnapshotItemModule {}
