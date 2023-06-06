ad3909df9624640d99edb9cef444db45effdee05
MinatoHikari <35342316+MinatoHikari@users.noreply.github.com>
2023-06-07 00:32:08 +0800

    auth return 401

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -3,9 +3,6 @@ import { Request, Response } from 'express';
 export function auth(req: Request, res: Response, next: () => void) {
   const token = req.header('Authorization');
   if (process.env.token !== token) {
-    res.send({
-      code: 40100,
-      message: 'token 校验失败',
-    });
+    res.status(401).send('auth failed');
   } else next();
 }
