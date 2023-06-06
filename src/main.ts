65096f6b56ffe3bf3c1c024f4cce5db124dee84a
MinatoHikari <35342316+MinatoHikari@users.noreply.github.com>
2023-06-06 23:50:38 +0800

    function midddleware

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './interceptor/transform/transform.interceptor';
 import { HttpExceptionFilter } from './filters/http-exception/http-exception.filter';
-import { AuthMiddleware } from './middleware/auth.middleware';
+import { auth } from './middleware/auth.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
-  app.use(AuthMiddleware);
+  app.use(auth);
   await app.listen(3000);
 }
 bootstrap();
--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,11 +1,6 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
 
-@Injectable()
-export class AuthMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: () => void) {
-    console.log(req.header('Authorization'));
-
-    next();
-  }
+export function auth(req: Request, res: Response, next: () => void) {
+  console.log(req.header('Authorization'));
+  next();
 }
